da1676d88d4afffa665ba00144cf150850246e38
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2018-05-23 16:13:59 +0200

    BF: Fix test_register

--- datalad_hirni/tests/test_register.py
+++ datalad_hirni/tests/test_register.py
@@ -3,9 +3,8 @@ from datalad.tests.utils import assert_result_count
 
 def test_register():
     import datalad.api as da
-    assert hasattr(da, 'hello_py')
-    assert_result_count(
-        da.hello_py(),
-        1,
-        action='demo')
+    assert hasattr(da, 'hirni_create_study')
+    assert hasattr(da, 'hirni_import_dcm')
+    assert hasattr(da, 'hirni_dicom2spec')
+    assert hasattr(da, 'hirni_spec2bids')
 
