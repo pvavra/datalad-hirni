28ebabfe4dc3e5a2bfe6ff46f6c4d22d5a51090e
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2018-08-29 15:05:55 +0200

    BF: Name conflict

--- datalad_hirni/commands/dicom2spec.py
+++ datalad_hirni/commands/dicom2spec.py
@@ -245,22 +245,22 @@ class Dicom2Spec(Interface):
         # ignore duplicates (prob. reruns of aborted runs)
         # -> convert highest id only
         import datalad_hirni.support.hirni_heuristic as heuristic
-        spec = sorted(spec_series_list,
-                      key=lambda x: heuristic.get_specval(x, 'id'))
-        for i in range(len(spec)):
-            if spec[i]["type"] == "dicomseries" and \
-                    heuristic.has_specval(spec[i], "converter") and \
-                            heuristic.get_specval(spec[i], "bids_run") in \
+        spec_series_list = sorted(spec_series_list,
+                                  key=lambda x: heuristic.get_specval(x, 'id'))
+        for i in range(len(spec_series_list)):
+            if spec_series_list[i]["type"] == "dicomseries" and \
+                    heuristic.has_specval(spec_series_list[i], "converter") and \
+                            heuristic.get_specval(spec_series_list[i], "bids_run") in \
                             [heuristic.get_specval(s, "bids_run")
-                             for s in spec[i + 1:]
+                             for s in spec_series_list[i + 1:]
                              if heuristic.get_specval(s,
                                                       "description") == heuristic.get_specval(
-                                    spec[i], "description") and \
+                                    spec_series_list[i], "description") and \
                                              heuristic.get_specval(s,
                                                                    "id") > heuristic.get_specval(
-                                             spec[i], "id")]:
+                                             spec_series_list[i], "id")]:
                 lgr.debug("Set converter to None for SeriesNumber %s" % i)
-                spec[i]["converter"] = dict(approved=True, value=None)
+                spec_series_list[i]["converter"] = dict(approved=True, value=None)
 
         lgr.debug("Storing specification (%s)", spec)
         # store as a stream (one record per file) to be able to
